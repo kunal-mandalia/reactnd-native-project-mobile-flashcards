a0b72ce36142e7090e8fc2edd8b33044c6fe69af
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-11-02 00:45:37 +0000

    feat: add Question component to Quiz

--- components/QuizView.js
+++ components/QuizView.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { Button } from './Common'
+
+const Progress = ({ activeQuestion, totalQuestions }) => (
+  <Text>{activeQuestion} / {totalQuestions}</Text>
+)
+
+const Question = ({ question, answer, showAnswer, onShowAnswer, onHideAnswer, onAnswer}) => {
+  return (
+    <View>
+      <Text>{showAnswer ? `Answer: ${answer}` :  `Question: ${question}`}</Text>
+      <Button title='show answer' onPress={onShowAnswer} />
+      <Button title='hide answer' onPress={onHideAnswer} />
+      <Button title='answer - incorrect' onPress={() => { onAnswer(false) }} />
+      <Button title='answer - correct' onPress={() => { onAnswer(true) }} />
+    </View>
+  )
+}
+
+// TODO: <Result onRetry showDetails />
 
 class QuizView extends Component {
   constructor (props) {
     super(props)
     this.state = {
       activeQuestion: 0,
-      answers: []
+      myAnswers: [],
+      questions: props.navigation.state.params.deck.questions,
+      showAnswer: false,
     }
   }
 
+  onShowAnswer = () => { this.setState({ showAnswer: true })}
+  onHideAnswer = () => { this.setState({ showAnswer: false })}
+  onAnswer = (isCorrect) => {
+    this.setState(state => ({
+      myAnswers: state.myAnswers.concat(isCorrect),
+      activeQuestion: state.activeQuestion + 1
+    }))
+  }
+
   render () {
-    const { activeQuestion, answers } = this.state
+    const { activeQuestion, myAnswers, showAnswer } = this.state
+    const { deck } = this.props.navigation.state.params
+    const { title, questions } = deck
     return (
       <View>
         <Text>QuizView</Text>
+        <Text>{JSON.stringify(this.state)}</Text>
+        <Progress activeQuestion={activeQuestion + 1} totalQuestions={deck.questions.length} />
+        <Question
+          question={questions[0].question}
+          answer={questions[0].answer}
+          showAnswer={showAnswer}
+          onShowAnswer={this.onShowAnswer}
+          onHideAnswer={this.onHideAnswer}
+          onAnswer={this.onAnswer}
+        />
       </View>
     )
   }
