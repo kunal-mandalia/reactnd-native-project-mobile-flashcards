4ebdc1cf742bd6e99f3e5c08ecb5736519584ee7
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-11-12 21:22:35 +0000

    chore: remove comments

--- components/IndividualDeckView.js
+++ components/IndividualDeckView.js
@@ -6,31 +6,6 @@ import Space from './Common/Space'
 import Title from './Common/Title'
 import { connect } from 'react-redux'
 
-// Convert to class and get deck from store
-// navigationOptions set within lifecycle? if not, use title
-// const IndividualDeckView = ({ navigation }) => {
-//   const { deck } = navigation.state.params
-//   return (
-//     <CoreLayout marginTop>
-//       <Button
-//         width='80%'
-//         title='Add Card'
-//         onPress={() => { navigation.navigate('NewQuestionView', { deck })}}
-//       />
-//       <Space />
-//       <Button
-//         width='80%'
-//         title='Start Quiz'
-//         onPress={() => { navigation.navigate('QuizView', { deck })}}
-//         disabled={deck.questions.length === 0}
-//       />
-//       <Space />
-//       <Title size={16} text={`${deck.questions.length} ${deck.questions.length === 1 ? 'card' : 'cards'}`} />
-//     </CoreLayout>
-//   )
-// }
-// IndividualDeckView.navigationOptions = ({ navigation }) => ({ title: navigation.state.params.deck.title || 'Deck' })
-
 class IndividualDeckView extends Component {
   static navigationOptions = ({ navigation }) => ({ title: navigation.state.params.deckTitle || 'Deck' })  
   constructor (props) {
