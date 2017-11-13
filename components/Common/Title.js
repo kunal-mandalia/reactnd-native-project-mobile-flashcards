7331a8151ecbcd0e31b0d810127078dbf7f3ea4e
kunal-mandalia <kunal.v.mandalia@gmail.com>
2017-11-13 02:22:24 +0000

    feat: align center

--- components/Common/Title.js
+++ components/Common/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text } from 'react-native'
 
 const Title = ({ text, size }) => (
-  <Text style={{ fontSize: size || 32, marginTop: 12, marginBottom: 24 }}>{text}</Text>
+  <Text style={{ fontSize: size || 32, marginTop: 12, marginBottom: 24, textAlign: 'center' }}>{text}</Text>
 )
 
 export default Title
